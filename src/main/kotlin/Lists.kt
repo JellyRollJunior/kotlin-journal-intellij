2c705e923c2458cda039b0fcceb61b6e572a6781
Brandon Lin <brandonlin9716@gmail.com>
2022-04-07 21:42:26 -0700

    Unit 2: Pathway 3 - part 2 Lists continued
    Lists and classes practice

--- src/main/kotlin/Lists.kt
+++ src/main/kotlin/Lists.kt
@@ -23,12 +23,14 @@ class Vegetables(vararg val toppings: String) : Item("Vegetables", 5) {
 class Order(val orderNumber: Int) {
     private val itemList = mutableListOf<Item>()
 
-    fun addItem(item: Item) {
+    fun addItem(item: Item): Order {
         itemList.add(item)
+        return this
     }
 
-    fun addAll(newItems: List<Item>) {
+    fun addAll(newItems: List<Item>): Order {
         itemList.addAll(newItems)
+        return this
     }
 
     fun print() {
@@ -44,21 +46,34 @@ class Order(val orderNumber: Int) {
 }
 
 fun main() {
+    val orderList = mutableListOf<Order>()
+
     val order1 = Order(1)
     order1.addItem(Noodles())
-    order1.print()
-
-    println()
+    orderList.add(order1)
 
     val order2 = Order(2)
     order2.addItem(Noodles())
     order2.addItem(Vegetables())
-    order2.print()
-
-    println()
+    orderList.add(order2)
 
     val order3 = Order(3)
     val items = listOf(Noodles(), Vegetables("Carrots", "Beans", "Celery"))
     order3.addAll(items)
-    order3.print()
+    orderList.add(order3)
+
+    val order4 = Order(4).addItem(Noodles()).addItem(Vegetables("Cabbage, Onion"))
+    orderList.add(order4)
+
+    orderList.add(
+        Order(5)
+            .addItem(Noodles())
+            .addItem(Noodles())
+            .addItem(Vegetables("Spinach"))
+    )
+
+    for (order in orderList) {
+        order.print()
+        println()
+    }
 }
